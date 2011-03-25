a42e9c23b1cddf5b58dbf15e3d726157ab36b31b
dongre avinash <dongre.avinash@gmail.com>
2011-03-25 19:58:24 +0530

    Adding support for binary search tree

--- test/t_clib.c
+++ test/t_clib.c
@@ -7,10 +7,10 @@ extern void test_c_rb();
 extern void test_c_set();
 
 int main( int argc, char**argv ) {	
-    /*test_c_array();
-      test_c_deque(500);
-      test_c_bst(); */
+    test_c_array();
+    test_c_deque(500);
+    test_c_bst(); 
     test_c_rb();
-    /*test_c_set();*/
+    test_c_set();
     return 0;
 }
